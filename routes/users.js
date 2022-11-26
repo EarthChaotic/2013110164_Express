462ab44e967c465cbaf61245ec2e29469439ce87
Jirapon Tresup <2013110164@TNISTUDENT.AC.TH>
2022-11-26 11:22:45 +0700

    WorkShopA

--- routes/users.js
+++ routes/users.js
@@ -3,7 +3,22 @@ var router = express.Router();
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+//res.send('Hello im under dhe wader please help me');
+res.status(200).json({
+  fullname:'Jirapon Tresup'
+  })
 });
+router.get('/bio', function(req, res, next) {
+  res.status(200).json(
+    {
+    fullname:'Jirapon Tresup',
+    Nickname:'Earth',
+    hobby:'Gaming',
+    gitusername:'EarthChaotic'
+    }
+    )
+  });
+
+
 
 module.exports = router;

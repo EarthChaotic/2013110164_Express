eb408bc232832503c488b5ad0844681f0939361a
Jirapon Tresup <2013110164@TNISTUDENT.AC.TH>
2022-12-10 16:04:40 +0700

    WorkshopB

--- controllers/conpanyController.js
+++ controllers/conpanyController.js
@@ -1,38 +1,112 @@
 const Company = require('../models/companys')
+
 exports.index = async (req, res, next) => {
-
-  const company = await Company.findOne()
-
+  const company = await Company.find()
     res.status(200).json({
       data:company
     });
-      //     data:[
-      //         {
-      //         id:1,
-      //         name:'Kojima Productions',
-      //         address:{
-      //           province:'Shinagawa',
-      //           postalcode:'140-8715',
-      //         },
-      //     },
-      //     {
-      //         id:2,
-      //         name:'Pearl Abyss',
-      //         address:{
-      //           province:'Shinagawa',
-      //           postalcode:'13900',
-      //         },
-      //     },
-      //     {
-      //         id:3,
-      //         name:'GRAVITY GAME TECH',
-      //         address:{
-      //           province:'Bangkok',
-      //           postalcode:'10500',
-      //         },
-      //     }
-      // ]
-
-
 };
   
+exports.insert = async (req, res, next) => {
+
+  const {name,address:{province}} = req.body
+  
+  let company = new Company({
+    name:name,
+    address:{
+      province:province
+    }
+  })
+  await company.save()
+    console.log(company)
+      res.status(200).json({
+        message:"เพิ่มข้อมูลเรียบร้อยแล้ว"
+      });
+  };
+
+exports.show = async (req, res, next) => {
+      try{
+          const{id} = req.params
+      
+          const company = await Company.findOne({
+              _id : id
+          })
+      
+          if(!company){
+              throw new Error('ไม่พบข้อมูล')
+          }
+          else{
+              res.status(200).json({
+                  data:company
+                });
+          }
+      }
+      catch(Error)
+      {
+          res.status(400).json({
+              error:{
+                  message:'เกิดข้อผิดพลาด ' + Error.message
+              }
+            });
+      }
+  };
+
+exports.destroy = async (req, res, next) => {
+      try{
+          const {id} = req.params 
+          const company = await Company.deleteOne({
+              _id:id
+          })
+          if(company.deletedCount === 0){
+              throw new Error('ไม่สามารถลบข้อมูลได้ / ไม่พบข้อมูล')
+          }else{
+              res.status(200).json({
+                  message:'ลบข้อมูลเรียบร้อยแล้ว'
+                });      
+          }
+      }
+      catch(Error)
+      {
+          res.status(400).json({
+              error:{
+                  message:'เกิดข้อผิดพลาด ' + Error.message
+              }
+            });
+      }
+  };
+  
+exports.update = async (req, res, next) => {
+  try{
+      const{id} = req.params
+      const{name,address:{province}}= req.body;
+
+      //Find By ID
+      // const company = await Company.findById(id);
+      // company.name = name
+      // company.salary = salary
+      // await company.save()
+
+      //Find by ID and Update
+      // const company = await Company.findByIdAndUpdate(id, {
+      //     name: name,
+      //     salary: salary,
+      //     });
+
+
+      const company = await Company.updateOne({ _id: id },{
+          name: name,
+          address:{province:province},
+          });
+      res.status(200).json({
+          message:'แก้ไขข้อมูลเรียบร้อยแล้ว'
+        });   
+  }
+  catch(Error)
+  {
+      res.status(400).json({
+          error:{
+              message:'เกิดข้อผิดพลาด ' + Error.message
+          }
+        });
+  }
+  };
--- routes/company.js
+++ routes/company.js
@@ -4,7 +4,10 @@ const conpanyController = require('../controllers/conpanyController')
 
 /* GET users listing. */
 router.get('/', conpanyController.index )
-
+router.post('/',conpanyController.insert)
+router.get('/:id', conpanyController.show )
+router.delete('/:id', conpanyController.destroy )
+router.put('/:id', conpanyController.update )
 
 
 

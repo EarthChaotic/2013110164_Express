2842bfee1f6f354b0f033bd3d22ff64f399a2d34
Jirapon Tresup <2013110164@TNISTUDENT.AC.TH>
2023-01-07 09:56:15 +0700

    Workshop A

--- app.js
+++ app.js
@@ -3,7 +3,8 @@ var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 const mongoose = require('mongoose');
-mongoose.connect('mongodb+srv://EarthChaotic:earth2102@2013110164-jirapon.z3xaafs.mongodb.net/RestfulAPI?retryWrites=true&w=majority',{useNewUrlParser: true, useUnifiedTopology: true})
+const config = require('./config/index');
+mongoose.connect(config.MONGODB_URI,{useNewUrlParser: true, useUnifiedTopology: true})
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
